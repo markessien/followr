b18a5b7501dae5de47e0c11b9487c7398c608364
Mark Essien <markessien@gmail.com>
2021-01-23 14:23:53 +0100

    Added basic view

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # followr
 A follow, unfollow tool
+
+
+
+## Persons website:
+
+- Calls an endpoint with javascript and specifies their domain. This endpoint returns a properly formated timeline. This is read only access.
+
+## Chrome Extension
+
+- Requires an API key that is entered in the extension.
+- Can follow be used to follow any site that has an RSS feed
+- Potentially can insert a follow link on any known site
+
+
+## Website
+
+- Very simple site like this https://plumebio.com/
+- You create a username and password and save.
+- You have an empty timeline then
+- You can add the rss feed of any site
+- You see an on-page timeline with the feeds
+- There is JS available that allows you embed this feed on your own website
--- followr.go
+++ followr.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+)
+
+//Create a struct that holds information to be displayed in our HTML file
+type Welcome struct {
+	Name string
+	Time string
+}
 
 func main() {
-	fmt.Println("hello world")
+
+	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
+	templates := template.Must(template.ParseFiles("templates/index.html"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		if name := r.FormValue("name"); name != "" {
+			welcome.Name = name
+		}
+
+		if err := templates.ExecuteTemplate(w, "index.html", welcome); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
+	//Print any errors from starting the webserver using fmt
+	fmt.Println("Listening")
+	fmt.Println(http.ListenAndServe(":8080", nil))
+
 }
